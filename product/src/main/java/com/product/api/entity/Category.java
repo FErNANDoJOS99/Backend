0d3f983ab6f665051935691c06aa54bdb34e001d
Cervantes Duarte Jose Fernando <licfernandoc@ciencias.unam.mx>
2025-02-27 17:32:57 -0600

    Funcionando sin base tal como practica 2.

---
+++ product/src/main/java/com/product/api/entity/Category.java
@@ -0,0 +1,47 @@
+package com.product.api.entity;
+
+
+public class Category {
+
+    Integer category_id;
+    String category;
+    String tag;
+    Integer status;
+
+
+    public Category(Integer id ,String category1 , String tag1,Integer status1 ){
+
+
+        category_id=id;
+        category= category1;
+        tag=tag1;
+        status=status1;
+    }
+
+
+     public String toString(){
+     
+        return "{"+category_id+","+category+","+tag+","+status+"}";
+     }
+
+
+   
+
+    public void changeStatus(){
+            status=0;
+    }
+
+    public String getcategory(){
+        return category;
+    }
+
+
+    public String gettag(){
+        return tag;
+    }
+    
+    public Integer getCategoryId() {
+    	return category_id;
+    }
+}
+
